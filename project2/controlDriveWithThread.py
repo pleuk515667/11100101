547bcb4c2f59d37833d80553036d41e66900eaaa
pleuk515667 <pleuk5667@gmail.com>
2016-10-11 15:07:09 -0400

    Update controlDriveWithThread.py

--- project2/controlDriveWithThread.py
+++ project2/controlDriveWithThread.py
@@ -1,30 +1,54 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Gist
- @pleuk515667
- Watch 0
-  Star 0
-  Fork 1 pleuk515667/CSE274-Project-1
- Code  Issues 0  Pull requests 0  Projects 1  Wiki  Pulse  Graphs  Settings
-CSE274-Project-1/project2/ 
-Name your file…
- or cancel
-    
- Edit new file    Preview
-1
-​
-@pleuk515667
-Commit new file
+import interfaceRobotPK
+import time
+import struct
+import math
+import random
+import threading
+r = interfaceRobotPK.Robot()
 
 
-Create new file
+check = 0
+left = 0
+right = 0
+def running():
+		global check
+		while 1:
+			if(check == 1):
+				print left
+				print right
+				r.driveDirect(left,right)
+			if(check == 0):
+				r.driveDirect(0,0)
+		
+def sensorCheck():
+		global check
+		global left
+		global right
+		while 1:
+			left = random.randint(-5,5)
+			right = random.randint(-5,5)
+			check = 1
+			time.sleep(5)
+			check = 0;
+			time.sleep(5)
+		
 
-Add an optional extended description…
-  Commit directly to the master branch.
-  Create a new branch for this commit and start a pull request. Learn more about pull requests.
-Commit new file  Cancel
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Status Help
+
+def Main():
+	r.toStop()
+	time.sleep(2)
+	r.toReset()
+	time.sleep(2)
+	r.toStart()
+	time.sleep(2)
+	r.toSafe()
+	time.sleep(2)
+	print "about to start"
+	time.sleep(2)
+	t1 = threading.Thread(name='running', target = running)
+	t2 = threading.Thread(name='sensorCheck', target = sensorCheck)
+	t1.start()
+	t2.start()
+	r.toStop()
+if __name__ == '__main__':
+	Main()
