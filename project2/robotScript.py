907eb16607a2023b9273c77942bcbdf0f68b4211
belegrin <belegrin@email.sc.edu>
2016-10-13 18:24:08 -0400

    Add files via upload

--- project2/robotScript.py
+++ project2/robotScript.py
@@ -1,45 +1,117 @@
-import interfaceRobot
+import interfaceRobotPK
 import time
 import struct
 import math
-r = interfaceRobot.Robot()
+import random
+import threading
+r = interfaceRobotPK.Robot()
+lock = threading.Lock()
+check = 99
+buttonState = 0
+def running():
+		global check
+		while 1:
+			if(check == 1):
+				
+				x = random.randint(5,10) *2
+				y = random.randint(5,10) *2
+				print "runing"
+				print x
+				print y
+				lock.acquire()
+				r.driveDirect(x,y)
+				lock.release()
+				check = 99
+			if(check == 0):
+				print "running 0, 0"
+				lock.acquire()
+				r.driveDirect(0,0)
+				time.sleep(1)
+				r.playWarningSong()
+				time.sleep(3)
+				lock.release()
+				check = 99
+			if (check ==2):
+				lock.acquire()
+				r.driveDirect(0,0)
+				time.sleep(0.3)
+				r.go(-5,0)
+				time.sleep(1)
+				x = random.randint(5,10) *2
+				y = random.randint(3,5) *2
+				r.driveDirect(x,y)
+				lock.release()
+				check = 99
+			if (check == 3):
+				lock.acquire()
+				r.driveDirect(0,0)
+				time.sleep(0.3)
+				r.go(-5,0)
+				time.sleep(1)
+				x = random.randint(3,5) *2
+				y = random.randint(5,10) *2
+				r.driveDirect(x,y)
+				lock.release()
+				check = 99
+		
+def sensorCheck():
+		global check
+		while 1:
+			lock.acquire()
+			byte = r.readingBumpWheel()
+			lock.release()
+			b1 = byte[3]
+			b2 = byte[2]
+			if(b1 == '1'):  
+				check = 2
+			if(b2 == '1'):
+				check = 3
+			time.sleep(0.015)
 
-def move(speed=0, radian=0, waitTime=1):
-	r.go(speed,radian)
-	time.sleep(waitTime)
-	r.go(0,0)
-	time.sleep(1)
+def buttonPressCheck(): 
+		global check
+		global buttonState
+		while 1:
+			lock.acquire()
+			byte = r.readingButton()
+			lock.release()
+			button = byte[3]
+			print button
+			if(button == '1' and buttonState == 0): 
+				check = 1
+				time.sleep(1)
+				buttonState = 1
+				button = 0
+			if(button == '1' and buttonState == 1): 
+				check = 0
+				time.sleep(1)
+				buttonState = 0
+			time.sleep(0.015)
 
-turning = 27.5
-pi = math.pi
-dT = 2.5
 
-degree  = math.radians(turning)
-theTime = (pi / 2) / degree
 time.sleep(2)
+print "Stop"
 r.toStop()
 time.sleep(2)
-print "Starting robot..."
+print "Start"
 r.toStart()
 time.sleep(2)
-print "Entering Safe mode..."
+print "Full"
 r.toSafe()
 time.sleep(2)
-bit = 0
-while bit != 1:
-	r.writeCommand(142)
-	r.writeCommand(18)
-	bit = r.readStatus()
-	print bit
-time.sleep(2)
-move(0,0,1)
-move(10,0,dT)
-move(0,30,theTime)
-move(10,0,dT)
-move(0,30,theTime)
-move(10,0,dT)
-move(0,30,theTime)
-move(10,0,dT)
-move(0,30,theTime)
-r.portClose()
+r.setStartSong()
+time.sleep(1)
+r.setWarningSong()
+time.sleep(1)
+r.playStartSong()
+time.sleep(4)
+t1 = threading.Thread(name='running', target = running)
+t2 = threading.Thread(name='sensorCheck', target = sensorCheck)
+t3 = threading.Thread(name='buttonPressCheck', target = buttonPressCheck)
+t1.start()
+t2.start()
+t3.start()
 
+
+
+ 
