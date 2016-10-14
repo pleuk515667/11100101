72d99cb96f510ef3534f3f8ee2f786c97a697277
belegrin <belegrin@email.sc.edu>
2016-10-13 20:22:05 -0400

    Add files via upload

--- project2/interfaceRobotPK.py
+++ project2/interfaceRobotPK.py
@@ -3,115 +3,117 @@ import time
 import struct
 import math
 
+
 class Robot:
-    #task1: open port write port and read port
-    #initialize the robot class, connect the serial, and set the variable
+    # task1: open port write port and read port
+    # initialize the robot class, connect the serial, and set the variable
     def __init__(self):
         self.ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
         self.currentMode = 0.0
         self.Wheel = 235.0
         self.diameter = 72.0
 
-    #Opening serial port    
+    # Opening serial port
     def portOpen(self):
-    	self.ser.open()
+        self.ser.open()
 
-    #closing serial port
+    # closing serial port
     def portClose(self):
         self.ser.close()
-      
-    #write command to the port
-    def writeCommand(self,input):
+
+    # write command to the port
+    def writeCommand(self, input):
         self.ser.write(chr(input))
-    
-    #read command from the port
+
+    # read command from the port
     def readStatus(self):
         data = self.ser.read(1)
         byte = -1
         if (data != ''):
-            byte = struct.unpack('B',data)[0]
-        return byte           
-       
-    #go to start mode
+            byte = struct.unpack('B', data)[0]
+        return byte
+
+        # go to start mode
+
     def toStart(self):
         self.writeCommand(128)
         self.currentMode = 128
-        
-    #go to safe mode
+
+    # go to safe mode
     def toSafe(self):
         self.writeCommand(131)
         self.currentMode = 131
-        
-    #go to Full mode
+
+    # go to Full mode
     def toFull(self):
         self.writeCommand(132)
         self.currentMode = 132
-    
-    #go to reset mode
+
+    # go to reset mode
     def toReset(self):
         self.writeCommand(7)
         self.currentMode = 7
-    
-    #go to Stop mode
+
+    # go to Stop mode
     def toStop(self):
         self.writeCommand(173)
         self.currentMode = 173
-    
-    #convert to bynay string
-    def toBinaryString(self,input):
+
+    # convert to bynay string
+    def toBinaryString(self, input):
         s = ''.join([chr(input)])
         return s
 
     def readingBumpWheel(self):
-    		self.writeCommand(142)
-    		self.writeCommand(7)
-    		byte = self.readStatus()
-    		byte = "{0:04b}".format(byte)
-    		return byte
+        self.writeCommand(142)
+        self.writeCommand(7)
+        byte = self.readStatus()
+        byte = "{0:04b}".format(byte)
+        return byte
 
     def readingButton(self):
-    		self.writeCommand(142)
-    		self.writeCommand(18)
-    		byte = self.readStatus()
-    		byte = "{0:04b}".format(byte)
-    		return byte
-        
+        self.writeCommand(142)
+        self.writeCommand(18)
+        byte = self.readStatus()
+        byte = "{0:04b}".format(byte)
+        return byte
+
     def readingCliffLeft(self):
-    	  self.writeCommand(142)
-    	  self.writeCommand(9)
-    	  byte = self.readStatus()
-    	  return byte
-    
+        self.writeCommand(142)
+        self.writeCommand(9)
+        byte = self.readStatus()
+        return byte
+
     def readingCliffRight(self):
-    	  self.writeCommand(142)
-    	  self.writeCommand(12)
-    	  byte = self.readStatus()
-    	  return byte
-    
+        self.writeCommand(142)
+        self.writeCommand(12)
+        byte = self.readStatus()
+        return byte
+
     def readingCliffLeftFront(self):
-    	  self.writeCommand(142)
-    	  self.writeCommand(10)
-    	  byte = self.readStatus()
-    	  return byte
-    
-    def readingCliffLeftRight(self):
-    	  self.writeCommand(142)
-    	  self.writeCommand(11)
-    	  byte = self.readStatus()
-    	  return byte
-    
+        self.writeCommand(142)
+        self.writeCommand(10)
+        byte = self.readStatus()
+        return byte
+
+    def readingCliffRightFront(self):
+        self.writeCommand(142)
+        self.writeCommand(11)
+        byte = self.readStatus()
+        return byte
+
     def checkCliffs(self):
         status = 0
         status = self.readingCliffLeft() + self.readingCliffRight() + self.readingCliffLeftFront() + self.readingCliffRightFront()
-        
-        if(status > 0):
+
+        if (status > 0):
             return 1
-        
+
         return 0
-        
-    #This function will drive the robot by calling the drive function
-    #set speed and radius and turn clockwise and counter clockwise
-    def drive(self,speed, radius, turn):
+
+    # This function will drive the robot by calling the drive function
+    # set speed and radius and turn clockwise and counter clockwise
+    def drive(self, speed, radius, turn):
         if type(speed) != type(42):
             speed = int(speed)
         if type(radius) != type(42):
@@ -131,32 +133,33 @@ class Robot:
                 radius = -1
             else:
                 radius = 1
-        radiusHi, radiusLow =  self.splitTo2Byte(radius)
+        radiusHi, radiusLow = self.splitTo2Byte(radius)
         self.writeCommand(137)
         self.writeCommand(speedHi)
         self.writeCommand(speedLow)
         self.writeCommand(radiusHi)
         self.writeCommand(radiusLow)
-    #calling drive and sending data to drive which is a speed of the wheel and degree
-    def go(self,speed=0,deg=0):
+
+    # calling drive and sending data to drive which is a speed of the wheel and degree
+    def go(self, speed=0, deg=0):
         if speed == 0:
-            dirst= None
+            dirst = None
             rad = math.radians(deg)
             if rad >= 0:
                 dirst = 'CCW'
             else:
                 dirst = 'CW'
-            finalSpeed = math.fabs(rad) * (self.Wheel/2.0)
-            self.drive(finalSpeed,0,dirst)
+            finalSpeed = math.fabs(rad) * (self.Wheel / 2.0)
+            self.drive(finalSpeed, 0, dirst)
         elif deg == 0:
-            finalSpeed = 10.0*speed
+            finalSpeed = 10.0 * speed
             rad = 32767
-            self.drive(finalSpeed,rad,"CW")
+            self.drive(finalSpeed, rad, "CW")
         else:
-            dirst= None
+            dirst = None
             radSpeed = math.radians(deg)
-            finalSpeed = 10*speed
-            rad = finalSpeed/radSpeed
+            finalSpeed = 10 * speed
+            rad = finalSpeed / radSpeed
             if rad > 32767:
                 rad = 32767
             if rad < -32767:
@@ -165,12 +168,12 @@ class Robot:
                 dirst = 'CCW'
             else:
                 dirst = 'CW'
-            self.drive(finalSpeed, rad,dirst)
+            self.drive(finalSpeed, rad, dirst)
         return
 
-    def driveDirect(self,speedRight,speedLeft):
-        speedRight = speedRight*10
-        speedLeft = speedLeft*10
+    def driveDirect(self, speedRight, speedLeft):
+        speedRight = speedRight * 10
+        speedLeft = speedLeft * 10
         if speedRight < -500:
             speedRight = -500
         if speedRight > 500:
@@ -193,13 +196,12 @@ class Robot:
         self.writeCommand(4)
         self.writeCommand(40)
         self.writeCommand(32)
-        self.writeCommand(39)  
+        self.writeCommand(39)
         self.writeCommand(32)
         self.writeCommand(38)
         self.writeCommand(32)
-        self.writeCommand(37)  
+        self.writeCommand(37)
         self.writeCommand(64)
-
 
     def playWarningSong(self):
         self.writeCommand(141)
@@ -211,25 +213,22 @@ class Robot:
         self.writeCommand(4)
         self.writeCommand(86)
         self.writeCommand(32)
-        self.writeCommand(87)  
+        self.writeCommand(87)
         self.writeCommand(32)
         self.writeCommand(88)
         self.writeCommand(32)
-        self.writeCommand(96)  
+        self.writeCommand(96)
         self.writeCommand(64)
-
 
     def playStartSong(self):
         self.writeCommand(141)
         self.writeCommand(0)
 
-
-    #spit value in to high and low bitwise
-    def splitTo2Byte(self,value):
+    # spit value in to high and low bitwise
+    def splitTo2Byte(self, value):
         bitValue = 0
         if value >= 0:
             bitValue = value
         else:
-            bitValue = (1<<16) + value
+            bitValue = (1 << 16) + value
         return ((bitValue >> 8) & 0xFF, bitValue & 0xFF)
-   
--- project2/robotScript.py
+++ project2/robotScript.py
@@ -4,93 +4,137 @@ import struct
 import math
 import random
 import threading
+
 r = interfaceRobotPK.Robot()
 lock = threading.Lock()
 check = 99
 buttonState = 0
-def running():
-		global check
-		while 1:
-			if(check == 1):
-				
-				x = random.randint(5,10) *2
-				y = random.randint(5,10) *2
-				print "runing"
-				print x
-				print y
-				lock.acquire()
-				r.driveDirect(x,y)
-				lock.release()
-				check = 99
-			if(check == 0):
-				print "running 0, 0"
-				lock.acquire()
-				r.driveDirect(0,0)
-				time.sleep(1)
-				r.playWarningSong()
-				time.sleep(3)
-				lock.release()
-				check = 99
-			if (check ==2):
-				lock.acquire()
-				r.driveDirect(0,0)
-				time.sleep(0.3)
-				r.go(-5,0)
-				time.sleep(1)
-				x = random.randint(5,10) *2
-				y = random.randint(3,5) *2
-				r.driveDirect(x,y)
-				lock.release()
-				check = 99
-			if (check == 3):
-				lock.acquire()
-				r.driveDirect(0,0)
-				time.sleep(0.3)
-				r.go(-5,0)
-				time.sleep(1)
-				x = random.randint(3,5) *2
-				y = random.randint(5,10) *2
-				r.driveDirect(x,y)
-				lock.release()
-				check = 99
-		
-def sensorCheck():
-		global check
-		while 1:
-			lock.acquire()
-			byte = r.readingBumpWheel()
-			lock.release()
-			b1 = byte[3]
-			b2 = byte[2]
-			b3 = byte[1]
-			b4 = byte[0]
-			if(b1 == '1'):  
-				check = 2
-			if(b2 == '1'):
-				check = 3
-			if(b3 == '1' or b4 = '1'):
-				check = 4
-			time.sleep(0.015)
 
-def buttonPressCheck(): 
-		global check
-		global buttonState
-		while 1:
-			lock.acquire()
-			byte = r.readingButton()
-			lock.release()
-			button = byte[3]
-			print button
-			if(button == '1' and buttonState == 0): 
-				check = 1
-				time.sleep(1)
-				buttonState = 1
-				button = 0
-			if(button == '1' and buttonState == 1): 
-				check = 0
-				time.sleep(1)
-				buttonState = 0
-			time.sleep(0.015)
+
+def running():
+    global check
+    while 1:
+        if (check == 1):
+            x = random.randint(5, 10) * 2
+            y = random.randint(5, 10) * 2
+            print "runing"
+            print x
+            print y
+            lock.acquire()
+            r.driveDirect(x, y)
+            lock.release()
+            check = 99
+        if (check == 0):
+            print "running 0, 0"
+            lock.acquire()
+            r.driveDirect(0, 0)
+            time.sleep(1)
+            r.playWarningSong()
+            time.sleep(3)
+            lock.release()
+            check = 99
+        if (check == 2):
+            lock.acquire()
+            r.driveDirect(0, 0)
+            r.playWarningSong()
+            time.sleep(0.3)
+            r.go(-5, 0)
+            time.sleep(1)
+            x = random.randint(5, 10) * 2
+            y = random.randint(3, 5) * 2
+            r.driveDirect(x, y)
+            lock.release()
+            check = 99
+        if (check == 3):
+            lock.acquire()
+            r.driveDirect(0, 0)
+            r.playWarningSong()
+            time.sleep(0.3)
+            r.go(-5, 0)
+            time.sleep(1)
+            x = random.randint(3, 5) * 2
+            y = random.randint(5, 10) * 2
+            r.driveDirect(x, y)
+            lock.release()
+            check = 99
+        if (check == 4):
+            lock.aquire()
+            r.driveDirect(0, 0)
+            r.playWarningSong()
+            lock.release()
+            check = 99
+
+
+def sensorCheck():
+    global check
+    while 1:
+        lock.acquire()
+        byte = r.readingBumpWheel()
+        lock.release()
+        b1 = byte[3]
+        b2 = byte[2]
+        # b3 = byte[1]
+        # b4 = byte[0]
+        if (b1 == '1'):
+            check = 2
+        if (b2 == '1'):
+            check = 3
+        # if (b3 == '1' or b4 == '1'):
+        #    check = 4
+        time.sleep(0.015)
+
+
+def buttonPressCheck():
+    global check
+    global buttonState
+    while 1:
+        lock.acquire()
+        byte = r.readingButton()
+        lock.release()
+        button = byte[3]
+        print button
+        if (button == 1 and buttonState == 0):
+            print 'reached first if'
+            # if(r.checkCliffs() == 1):
+            #    check = 4
+            # else:
+            #    check = 1
+            check = 1
+            time.sleep(1)
+            buttonState = 1
+            button = 0
+        if (button == 1 and buttonState == 1):
+            print 'reached second if'
+            # if(r.checkCliffs() == 1):
+            #    check = 4
+            # else:
+            #    check = 0
+            check = 0
+            time.sleep(1)
+            buttonState = 0
+        print 'past ifs reached'
+        time.sleep(0.015)
+
+
+def cliffWheelCheck():
+    global check
+    global buttonState
+    while 1:
+
+        lock.acquire()
+        '''
+        cliffLeft = r.readingCliffLeft()
+        cliffRight = r.readingCliffRight()
+        cliffLeftFront = r.readingCliffLeftFront()
+        cliffRightFront = r.readingCliffRightFront()
+        '''
+        lock.release()
+        anyCliff = cliffLeft + cliffRight + cliffLeftFront + cliffRightFront
+        print anyCliff
+        if (anyCliff > 0):
+            check = 4
+
+        time.sleep(0.015)
 
 
 time.sleep(2)
@@ -109,13 +153,11 @@ r.setWarningSong()
 time.sleep(1)
 r.playStartSong()
 time.sleep(4)
-t1 = threading.Thread(name='running', target = running)
-t2 = threading.Thread(name='sensorCheck', target = sensorCheck)
-t3 = threading.Thread(name='buttonPressCheck', target = buttonPressCheck)
+t1 = threading.Thread(name='running', target=running)
+t2 = threading.Thread(name='sensorCheck', target=sensorCheck)
+t3 = threading.Thread(name='buttonPressCheck', target=buttonPressCheck)
+t4 = threading.Thread(name='cliffWheelCheck', target=cliffWheelCheck)
 t1.start()
 t2.start()
 t3.start()
-
-
-
- 
+t4.start()
